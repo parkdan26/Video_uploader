fab56c389fad3bd244a2c8a1d7ef576fe51032f6
Daniel Park <danyul@Daniels-Air-4.wlu.ca>
2025-02-25 20:11:05 -0500

    additions

--- text.py
+++ text.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyAzfRGFpTH27RnYn5-BKnuko-C14sLG5UM")
+client = genai.Client(api_key="")
 response = client.models.generate_content(
     model="gemini-2.0-flash", contents="Make a (am i the asshole) under 100 words, paragraph"
 )
